698bd79488bb2bfa4178b620289782d4683da5a8
valealfa14 <68080208+valealfa14@users.noreply.github.com>
2021-11-30 20:49:47 -0600

    Crear atributos
    Se crearon los atributos de las clases Gato y Numeros

---
+++ Gato.js
@@ -0,0 +1,5 @@
+//Variables
+let nombre = "blanca";
+let raza = "angora";
+
+alert("La gata " + nombre + " es de raza " + raza);
---
+++ Numeros.js
@@ -0,0 +1,15 @@
+let numero1 = 5;
+let numero2 = 10;
+
+ //alert("numero1 + numero2");
+
+ function suma(num1, num2){
+
+    let resultado = num1 + num2; //Que va a hacer la funcion?
+    return resultado // Me retorne el resultado
+
+ }
+
+ let total_de_suma = suma(20, 25); //Doy parametros para la funcion
+
+ document.write("La suma tiene como resultado " + total_de_suma); //Se muestre en pantalla
